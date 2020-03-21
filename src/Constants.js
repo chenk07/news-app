e55824a7b08955fda5185c1befa77171fb24b9a1
Kai <chen885@usc.edu>
2020-03-21 11:50:49 -0700

    update Azure AutoSuggest key

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 export const BACKEND_ROOT = 'http://csci571newsapp-env.eba-wmswsiyn.us-east-1.elasticbeanstalk.com/';
 export const AUTO_SUGGESTION_API_ROOT = 'https://api.cognitive.microsoft.com/bing/v7.0/suggestions?mkt=fr-FR&q=';
-export const AUTO_SUGGESTION_API_KEY = 'd1a76d9ed98740248839d5c33e9eb0ac';
+export const AUTO_SUGGESTION_API_KEY = '387a1f27b4304ab79c7d51db5f1baaab';
 export const GUARDIAN = 'guardian';
 export const NYT = 'nyt';
 export const TWITTER_HASH_TAG = ["CSCI_571_NewsApp"];
